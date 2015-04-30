14f54a779f7d6766c97c655fb671a30f63bfd2b0
Enricofindley <real96coffin@gmail.com>
2015-04-30 13:58:55 +0700

    GUI
    GUI For MIBD Rent

---
+++ MIBD Rent/src/Penyewa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author i13008
+ */
+public class Penyewa {
+    private String comment;
+    private String nama;
+    private String alamat;
+    
+    
+}
